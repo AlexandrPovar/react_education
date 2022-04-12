5316ea00d64eba346f57e803b49633eb1c76edcf
Aleksandr <povar.alexandr1901@gmail.com>
2022-04-12 21:09:43 +0300

    add onIncrement & onDecrement

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -16,31 +16,32 @@ const CountersList = () => {
     const newCounters = counters.filter((c) => c.id !== id);
     setCounters(newCounters);
   };
+  const handleIncrement = (value) => {
+    console.log("handleIncrement", value);
+    setCounters(value + 1);
+  };
+  const handleDecrement = (value) => {
+    const valueDecrement = value - 1;
+    setCounters(valueDecrement);
+  };
   const handleReset = () => {
     setCounters(initialState);
     console.log("handleReset");
-  };
-  const handleUpdate = () => {
-    const updatedlState = [
-      { id: 0, value: 2, name: "Ненужная вещь", price: "200" },
-      { id: 1, value: 3, name: "Ложка" },
-      { id: 2, value: 4, name: "Вилка" },
-      { id: 3, value: 5, name: "Тарелка" },
-      { id: 4, value: 0, name: "Надор минималиста" },
-    ];
-    setCounters(updatedlState);
   };
 
   return (
     <>
       {counters.map((count) => (
-        <Counter key={count.id} onDelete={handleDelete} {...count} />
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...count}
+        />
       ))}
       <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
         Сброс
-      </button>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleUpdate}>
-        Обновить состояние
       </button>
     </>
   );
